a66cae052073395cd7b25078fa9a78ba79b5430f
ashish <ashishmylavarapu123@msitprogram.net>
2021-08-16 09:57:13 +0530

    solved circleintersect

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -6,4 +6,11 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
-	return False 
+	d = (x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)
+	r = (r1+r2) * (r1+r2)
+	if (d<r):
+		return True
+	elif(d==r):
+		return True
+	else:
+		return False 
